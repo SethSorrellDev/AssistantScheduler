b791f06e5c55d415e58856358955fe96eb2bb7e5
SethTheMountain <smsorrell27@gmail.com>
2026-07-23 19:48:11 -0400

    Add seed to build script for automatic production seeding

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ pip install -r requirements.txt
 echo "--- Running database migrations ---"
 flask db upgrade
 
+echo "--- Seeding database ---"
+python seed_production.py
+
 echo "--- Build complete ---"
